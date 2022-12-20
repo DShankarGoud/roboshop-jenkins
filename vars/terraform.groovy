7f1301ff2cead350f5e7502089984cea07ad7971
shiva shankar <sivas1475@gmail.com>
2022-12-20 21:55:00 +0530

    JENKINS

---
+++ vars/terraform.groovy
@@ -0,0 +1,39 @@
+def call() {
+  pipeline {
+
+    agent any
+
+    options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'VERSION', defaultValue: '', description: 'Which Version?')
+        }
+
+        environment {
+            SSH = credentials('SSH')
+        }
+
+        stages {
+
+            stage ("Terraform INIT") {
+                steps{
+                    sh '''
+                      terraform init -backend-config=env-${ENV}/backend.tfvars
+                    '''
+                }
+            }
+
+            stage ("Terraform Apply") {
+                steps{
+                    sh '''
+                      terraform apply -auto-approve -var-file=env-${ENV}/${ENV}.tfvars
+                    '''
+                }
+            }
+
+        }
+    }
+}
